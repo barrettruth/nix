5ad9238a46351d61cb1d64777c807d30027ac67b
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-03 01:32:25 -0500

    ci: format

--- config/zen/user.js
+++ config/zen/user.js
@@ -15,7 +15,10 @@ user_pref("font.name.serif.x-western", "Times New Roman");
 user_pref("font.name.sans-serif.x-western", "SF Pro Display");
 user_pref("font.name.monospace.x-western", "Berkeley Mono");
 user_pref("font.name.serif.x-math", "STIX Two Math");
-user_pref("font.name-list.serif.x-math", "STIX Two Math, Latin Modern Math, Times New Roman");
+user_pref(
+  "font.name-list.serif.x-math",
+  "STIX Two Math, Latin Modern Math, Times New Roman",
+);
 user_pref("font.default.x-western", "sans-serif");
 
 user_pref("browser.tabs.insertAfterCurrent", true);
