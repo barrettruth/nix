c33a7983bd41a2144a8aa3663f62873feccb0880
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-20 13:27:14 -0400

    feat(nvim): buftypes

--- config/nvim/after/plugin/lsp/vimdoc_ls.lua
+++ config/nvim/after/plugin/lsp/vimdoc_ls.lua
@@ -1,5 +1,7 @@
 return {
     cmd = { 'vimdoc-language-server' },
     filetypes = { 'help' },
+    -- buftypes = { 'help' },
     root_markers = { 'doc', '.git' },
+    workspace_required = false,
 }
