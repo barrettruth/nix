2838e6c5c2c6f822ed7f76f29f777abee092a531
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-28 18:35:35 -0400

    feat(neovim): use forge as plugin

--- config/nvim/lua/plugins/dev.lua
+++ config/nvim/lua/plugins/dev.lua
@@ -25,6 +25,57 @@ local synctex_pdf = {}
 
 return {
     {
+        'barrettruth/forge.nvim',
+        enabled = true,
+        before = function()
+            vim.g.forge = {
+                ci = { lines = 5000 },
+                keys = {
+                    pr = {
+                        checkout = '<cr>',
+                        diff = '<c-d>',
+                        worktree = '<c-w>',
+                        ci = '<c-t>',
+                        browse = '<c-x>',
+                        manage = '<c-e>',
+                        create = '<c-a>',
+                        filter = '<c-o>',
+                        refresh = '<c-r>',
+                    },
+                    issue = {
+                        browse = '<cr>',
+                        close = '<c-s>',
+                        filter = '<c-o>',
+                        refresh = '<c-r>',
+                    },
+                    commits = {
+                        checkout = '<cr>',
+                        diff = '<c-d>',
+                        browse = '<c-x>',
+                        yank = '<c-y>',
+                    },
+                },
+                display = {
+                    widths = {
+                        title = 50,
+                        author = 12,
+                        name = 40,
+                        branch = 20,
+                    },
+                    limits = {
+                        pulls = 50,
+                        issues = 50,
+                        runs = 20,
+                    },
+                },
+            }
+        end,
+        after = function()
+            vim.cmd.packadd('fzf-lua')
+        end,
+        cmd = 'Forge',
+    },
+    {
         'barrettruth/diffs.nvim',
         enabled = true,
         before = function()
