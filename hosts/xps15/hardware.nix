6b766210b574a32cde15cffde1368af729fe3e5b
Barrett Ruth <br.barrettruth@gmail.com>
2026-04-03 21:07:30 -0400

    fix: reboot

--- hosts/xps15/hardware.nix
+++ hosts/xps15/hardware.nix
@@ -10,6 +10,7 @@
     open = false;
     modesetting.enable = true;
     powerManagement.enable = true;
+    powerManagement.finegrained = true;
     prime = {
       offload.enable = true;
       intelBusId = "PCI:0:2:0";
