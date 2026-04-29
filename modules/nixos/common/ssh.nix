6cf4bca1c44d21c23753ac40d6477115bf5a740f
Barrett Ruth <br.barrettruth@gmail.com>
2026-04-29 18:58:39 -0400

    feat(ssh): disable password

--- modules/nixos/common/ssh.nix
+++ modules/nixos/common/ssh.nix
@@ -1,3 +1,10 @@
 {
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      KbdInteractiveAuthentication = false;
+      PasswordAuthentication = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
 }
