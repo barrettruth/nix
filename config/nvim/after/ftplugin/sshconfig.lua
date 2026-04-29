4b3eeeaf0d4bbeaf2f24e93228ea4bdc3df11ade
Barrett Ruth <br.barrettruth@gmail.com>
2026-04-29 18:58:51 -0400

    feat(nvim): completion enhancements

--- config/nvim/after/ftplugin/sshconfig.lua
+++ config/nvim/after/ftplugin/sshconfig.lua
@@ -1,6 +1,6 @@
 local ssh = require('config.completion.ssh')
 
-vim.opt_local.complete = { '.', 'w', 'b' }
-vim.bo.omnifunc = "v:lua.require'config.completion.ssh'.complete"
-
-ssh.preload()
+require('config.completion.filetype').setup(
+    "v:lua.require'config.completion.ssh'.complete",
+    ssh.preload
+)
