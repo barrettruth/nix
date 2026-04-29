5b4097aaec707c6d2987dd8e9e29e44795f7c8ee
Barrett Ruth <br.barrettruth@gmail.com>
2026-04-29 18:30:53 -0400

    feat(nvim): completion enhancements

--- config/nvim/after/ftplugin/tmux.lua
+++ config/nvim/after/ftplugin/tmux.lua
@@ -1,6 +1,6 @@
 local tmux = require('config.completion.tmux')
 
-vim.opt_local.complete = { 'o', '.', 'w', 'b' }
+vim.opt_local.complete = { '.', 'w', 'b' }
 vim.bo.omnifunc = "v:lua.require'config.completion.tmux'.complete"
 
 tmux.preload()
