e700ba323d1814f6b4f274d6ad98447d1b482263
Barrett Ruth <br.barrettruth@gmail.com>
2026-05-02 14:40:34 -0400

    feat(nix): gpg key setup

--- modules/nixos/desktop/services.nix
+++ modules/nixos/desktop/services.nix
@@ -19,11 +19,8 @@ lib.mkMerge [
       enable = true;
       pinentryPackage = pkgs.pinentry-curses;
       settings = {
-        # Largest safe positive signed 32-bit TTL, effectively infinite for a login session.
         default-cache-ttl = 2147483647;
         default-cache-ttl-ssh = 2147483647;
-
-        # Disable the creation-time maximum lifetime cap.
         max-cache-ttl = 0;
         max-cache-ttl-ssh = 0;
       };
