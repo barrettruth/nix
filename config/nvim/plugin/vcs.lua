0a0fea39f319b0bdeb1aaa844065473eae99bb0c
Barrett Ruth <br@barrettruth.com>
2026-06-27 23:56:39 -0500

    fix(nvim): move guh to proper file

--- config/nvim/plugin/vcs.lua
+++ config/nvim/plugin/vcs.lua
@@ -1,3 +1,20 @@
 vim.pack.add({
     'https://github.com/tpope/vim-fugitive',
 })
+
+vim.pack.add({
+    'https://github.com/justinmk/guh.nvim',
+}, { load = function() end })
+
+require('lz.n').load({
+    {
+        'justinmk/guh.nvim',
+        cmd = { 'Guh', 'GuhComment' },
+        keys = {
+            { '<leader>gg', '<cmd>Guh<cr>', desc = 'guh repo' },
+            { '<leader>go', '<cmd>Guh .<cr>', desc = 'guh open target' },
+            { '<leader>gr', '<Plug>(guh-review)', desc = 'guh review pr' },
+            { '<leader>gt', '<Plug>(guh-logs)', desc = 'guh pr logs' },
+        },
+    },
+})
