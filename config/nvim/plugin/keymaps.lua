48e19ebdd6c349b4ae3b9acecd127db11bd0602a
Barrett Ruth <br@barrettruth.com>
2026-07-14 23:06:12 -0500

    fix(nvim): remove already-bound mappings

--- config/nvim/plugin/keymaps.lua
+++ config/nvim/plugin/keymaps.lua
@@ -31,9 +31,6 @@ for _, key in ipairs({ 'h', 'j', 'k', 'l' }) do
         { desc = 'go to window ' .. key }
     )
 end
-vim.keymap.set('n', '[t', '<cmd>tabprevious<cr>', { desc = 'previous tab' })
-vim.keymap.set('n', ']t', '<cmd>tabnext<cr>', { desc = 'next tab' })
-
 vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'join lines (keep cursor)' })
 
 vim.keymap.set('x', 'p', '"_dp', { desc = 'paste without yanking' })
