652b65db9d94f70b00ecf7e66099043efcb9c3a1
Barrett Ruth <br@barrettruth.com>
2026-08-13 21:42:37 -0500

    retired: join the tailnet outbound-only

--- modules/identity.nix
+++ modules/identity.nix
@@ -20,6 +20,7 @@ let
     sshKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILA1pOJawzHtJqIn56AZT4IhPUh9vUEhLPLwndk5s3iM ${email}"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAVzzY8UkHm+o6u8JIFJQNLnA8uCo7STUpvPXL70SQr ${email}"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILfDdIk0QwJo0FIkRQ2mE3PXw1bjPmtrGZhfJ2FeyoId ${email}"
     ];
   };
 in
