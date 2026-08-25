ab661af7a89f0dc8dbd9e782f8ebf2f8ef758c27
Barrett Ruth <br@barrettruth.com>
2026-08-25 23:44:35 +1000

    nvim: highlight bzl files with the starlark parser

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@ vim.pack.add({
     'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
 })
 
+vim.treesitter.language.register('starlark', 'bzl')
+
 local group = vim.api.nvim_create_augroup('ATreesitter', { clear = true })
 
 ---@param buf integer
