27e3a67a748c0b4e0de3ac00eb5f2478183abbe8
Barrett Ruth <br@barrettruth.com>
2026-08-29 17:17:26 +1000

    neovim: install the pinned nightly

--- modules/nixos/common/nix.nix
+++ modules/nixos/common/nix.nix
@@ -5,5 +5,9 @@
       "nix-command"
       "flakes"
     ];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 }
