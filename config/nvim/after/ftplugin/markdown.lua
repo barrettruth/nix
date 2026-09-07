1ac61e57176ab5e9b37323d98cf3f07811c6a93d
Barrett Ruth <br@barrettruth.com>
2026-09-07 16:35:26 +1000

    fix(nvim): clear Markdown metadata state

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,4 @@ vim.opt_local.textwidth = 80
 vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
     .. '\nsetl tw<'
     .. '\ncall v:lua.require("config.ftplugin").undo_window_options(["conceallevel", "foldexpr", "foldmethod", "foldtext"])'
+    .. '\nunl! b:html_omni_flavor b:yaml_schema'
